e16e98cd94b69a3caa18b924fd35328250674fc6
ayushdit03 <148032098+ayushdit03@users.noreply.github.com>
2024-07-03 21:01:12 +0530

    Update comingsoon.js

--- src/pages/comingsoon_page/comingsoon.js
+++ src/pages/comingsoon_page/comingsoon.js
@@ -3,9 +3,9 @@ import Particle from '../../Particle';
 import { Container, Row, Col } from 'react-bootstrap';
 import Zoom from 'react-reveal/Zoom';
 import { Fade } from 'react-reveal';
-import gfg from '../../images/gfg.png';
-import ay from '../../images/ay-py.png';
-import cbta from '../../images/UC-ced27dc4-f236-4bd7-b1c1-d93fdff0f3e2.pdf';
+import dpu from '../../images/dpu.png';
+import zion from '../../images/zion.png';
+import dental from '../../images/dental.png';
 
 export default function ComingSoon() {
     return (
@@ -29,27 +29,27 @@ export default function ComingSoon() {
                                                     <div className="cert-card" style={{ height: 'calc(100% + 40px)' }}>
                                                         <div className="content">
                                                             <a
-                                                                href="https://media.geeksforgeeks.org/courses/certificates/29c356e06c745af62c8535beb06adb4c.pdf"
+                                                                href="https://www.linkedin.com/posts/meet-agarwal-720160228_we-won-a-24-hrs-hackathon-devclash-ugcPost-7182259165526978560-5kPx?utm_source=share&utm_medium=member_desktop"
                                                                 target="_blank"
                                                                 rel="noopener noreferrer"
                                                             >
                                                                 <div className="content-overlay"></div>
                                                                 <div className="cert-header" style={{ backgroundColor: "white" }}>
-                                                                    <img className="logo_img" src={gfg} alt={gfg} />
+                                                                    <img className="logo_img" src={dpu} alt={dpu} />
                                                                 </div>
                                                                 <div className="content-details fadeIn-top">
                                                                     <h3 className="content-title" style={{ color: "black" }}>
-                                                                        Certificate
+                                                                        2024
                                                                     </h3>
                                                                 </div>
                                                             </a>
                                                         </div>
                                                         <div className="cert-body">
                                                             <h2 className="cert-body-title" style={{ fontWeight: 700, color: "#fbd9ad" }}>
-                                                                Complete Machine Learning And Data Science Program
+                                                                Got Developer Of The Year Title for year 2024
                                                             </h2>
                                                             <h3 className="cert-body-subtitle" style={{ color: "#eb90ff", marginBottom: "0px" }}>
-                                                                - GeeksForGeeks
+                                                                - Devcraft
                                                             </h3>
                                                         </div>
                                                     </div>
@@ -60,27 +60,27 @@ export default function ComingSoon() {
                                                     <div className="cert-card" style={{ height: 'calc(100% + 40px)' }}>
                                                         <div className="content">
                                                             <a
-                                                                href={cbta}
+                                                                href="#"
                                                                 target="_blank"
                                                                 rel="noopener noreferrer"
                                                             >
                                                                 <div className="content-overlay"></div>
                                                                 <div className="cert-header" style={{ backgroundColor: "white" }}>
-                                                                    <img className="logo_img" src={ay} alt={ay} />
+                                                                    <img className="logo_img" src={dental} alt={dental} />
                                                                 </div>
                                                                 <div className="content-details fadeIn-top">
                                                                     <h3 className="content-title" style={{ color: "black" }}>
-                                                                        Certificate
+                                                                        it X dental
                                                                     </h3>
                                                                 </div>
                                                             </a>
                                                         </div>
                                                         <div className="cert-body">
                                                             <h2 className="cert-body-title" style={{ fontWeight: 700, color: "#fbd9ad" }}>
-                                                                Python for Data Science and Machine Learning
+                                                                Worked as a research project lead on collaboration with Dental Dpu college and Dpu IT college
                                                             </h2>
                                                             <h3 className="cert-body-subtitle" style={{ color: "#eb90ff", marginBottom: "0px" }}>
-                                                                - Udemy
+                                                                - DPU
                                                             </h3>
                                                         </div>
                                                     </div>
@@ -92,27 +92,27 @@ export default function ComingSoon() {
                                                     <div className="cert-card" style={{ height: 'calc(100% + 40px)' }}>
                                                         <div className="content">
                                                             <a
-                                                                href="https://media.geeksforgeeks.org/courses/certificates/29c356e06c745af62c8535beb06adb4c.pdf"
+                                                                href="https://www.linkedin.com/posts/meet-agarwal-720160228_we-won-a-24-hrs-hackathon-devclash-ugcPost-7182259165526978560-5kPx?utm_source=share&utm_medium=member_desktop"
                                                                 target="_blank"
                                                                 rel="noopener noreferrer"
                                                             >
                                                                 <div className="content-overlay"></div>
                                                                 <div className="cert-header" style={{ backgroundColor: "white" }}>
-                                                                    <img className="logo_img" src={gfg} alt={gfg} />
+                                                                    <img className="logo_img" src={dpu} alt={dpu} />
                                                                 </div>
                                                                 <div className="content-details fadeIn-top">
                                                                     <h3 className="content-title" style={{ color: "black" }}>
-                                                                        Certificate
+                                                                        Hackathon
                                                                     </h3>
                                                                 </div>
                                                             </a>
                                                         </div>
                                                         <div className="cert-body">
                                                             <h2 className="cert-body-title" style={{ fontWeight: 700, color: "#fbd9ad" }}>
-                                                                Complete Machine Learning And Data Science Program
+                                                                Won 24 hrs live hackathon 
                                                             </h2>
                                                             <h3 className="cert-body-subtitle" style={{ color: "#eb90ff", marginBottom: "0px" }}>
-                                                                - GeeksForGeeks
+                                                                - Devcraft
                                                             </h3>
                                                         </div>
                                                     </div>
@@ -123,27 +123,27 @@ export default function ComingSoon() {
                                                     <div className="cert-card" style={{ height: 'calc(100% + 40px)' }}>
                                                         <div className="content">
                                                             <a
-                                                                href={cbta}
+                                                                href="3"
                                                                 target="_blank"
                                                                 rel="noopener noreferrer"
                                                             >
                                                                 <div className="content-overlay"></div>
                                                                 <div className="cert-header" style={{ backgroundColor: "white" }}>
-                                                                    <img className="logo_img" src={ay} alt={ay} />
+                                                                    <img className="logo_img" src={zion} alt={zion} />
                                                                 </div>
                                                                 <div className="content-details fadeIn-top">
                                                                     <h3 className="content-title" style={{ color: "black" }}>
-                                                                        Certificate
+                                                                        Hackathon
                                                                     </h3>
                                                                 </div>
                                                             </a>
                                                         </div>
                                                         <div className="cert-body">
                                                             <h2 className="cert-body-title" style={{ fontWeight: 700, color: "#fbd9ad" }}>
-                                                                Python for Data Science and Machine Learning
+                                                                Ranked 3rd in  Web Development Hackathon
                                                             </h2>
                                                             <h3 className="cert-body-subtitle" style={{ color: "#eb90ff", marginBottom: "0px" }}>
-                                                                - Udemy
+                                                                - Zion
                                                             </h3>
                                                         </div>
                                                     </div>
